1293caf0ee01271acd13585c9b9de1ba583e038b
DawidPL <32899009+DawidPL@users.noreply.github.com>
2019-01-17 13:09:50 +0100

    Delete math.js

--- math.js
+++
@@ -1,8 +0,0 @@
-function userPassword(){
-  var data = document.getElementById("user_value");
-  document.getElementById("user_value_button").disabled = false;
-  while (data == null){
-  document.getElementById("user_value_button").disabled = true;
-  }
-  return data;
-}
